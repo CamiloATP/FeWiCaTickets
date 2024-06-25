60c96fd572a12a337d3ceae5c79758db2d3db993
Camilo.ATP <camilo.antonio.t@gmail.com>
2024-06-25 00:44:55 -0400

    first commit

---
+++ src/main/java/com/example/demo/FewicaTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FewicaTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FewicaTicketsApplication.class, args);
+	}
+
+}
